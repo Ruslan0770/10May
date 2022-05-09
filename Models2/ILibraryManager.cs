d6ac046ac0549c64dc4eefcda0a771866959f059
Ruslan0770 <ruslania@code.edu.az>
2022-05-09 15:59:47 -0700

    finalcommit

---
+++ Models2/ILibraryManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEWtask.Models2
+{
+    interface ILibraryManager
+    {
+    }
+}
---
+++ Models2/ServiceLibrary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEWtask.Models2
+{
+    class ServiceLibrary : ILibraryManager
+    {
+        public List<Book> Books;
+        public List<Book> Book => Books;
+
+        public void Add(Book book)
+        {
+            List<Book> books = new List<Book>();
+            foreach (Book _book in Books)
+            {
+                if (_book.Name.ToUpper() != book.Name.ToUpper())
+                {
+                    books.Add(book);
+                }
+
+            }
+            throw new Exception("SameBookalreadyAddedExpection");
+
+        }
+        public Book ShowInfo(string bookname)
+        {
+            foreach (Book book in Books)
+            {
+                if (book.Name.ToLower() == bookname.ToLower())
+                    return book;
+            }
+            throw new Exception("BookNotFoundException");
+        }
+        public List<Book> Search(Predicate<Book> predicate)
+        {
+            List<Book> books = new List<Book>();
+            foreach (Book book in Books)
+            {
+                if (predicate(book))
+                {
+                    books.Add(book);
+                }
+            }
+            return books;
+
+        }
+
+        public List<Book> Filter(string genre)
+        {
+            List<Book> books = new List<Book>();
+            foreach (Book book in books)
+            {
+                if (book.Genre.ToLower() == genre.ToLower())
+                {
+                    books.Add(book);
+                }
+            }
+            return books;
+
+        }
+
+
+    }
+
+
+
+}
+
